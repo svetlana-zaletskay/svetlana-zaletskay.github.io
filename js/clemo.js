be963f0e1a34a2799341ec4b158c6325f6e1bb44
Svetlana Zaletskaya <svetka-pipetka10@mail.ru>
2016-12-02 23:25:15 +0200

    2

--- js/clemo.js
+++ js/clemo.js
@@ -5,5 +5,13 @@ $(document).ready(function(){
 		slidesToShow: 3,
 		slidesToScroll: 3
 	});
+
+	$('.client-portfolio-slider').slick({
+		dots: true,
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4
+	});
 });
+
 
