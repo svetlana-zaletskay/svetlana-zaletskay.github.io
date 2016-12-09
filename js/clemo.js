253fcc717da87f35ab205a2280714e9a70a99549
Svetlana Zaletskaya <svetka-pipetka10@mail.ru>
2016-12-09 21:12:22 +0200

    3

--- js/clemo.js
+++ js/clemo.js
@@ -1,17 +1,81 @@
 $(document).ready(function(){
 	$('.team-slider').slick({
 		dots: true,
-		infinite: true,
+		infinite: false,
 		slidesToShow: 3,
-		slidesToScroll: 3
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+            {
+                breakpoint: 667,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+
+		]
 	});
 
 	$('.client-portfolio-slider').slick({
 		dots: true,
-		infinite: true,
+		infinite: false,
 		slidesToShow: 4,
-		slidesToScroll: 4
+		slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+
+                }
+            },
+            {
+                breakpoint: 667,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
 	});
+
+	$('#mobileMenuButton').click(function () {
+		$(this).toggleClass('nav-mobile-button');
+		$('.nav').toggleClass('nav-mobile');
+		$('.link-nav').toggleClass('nav-el-mobile');
+    })
+
+	$('.link-nav').click(function () {
+        $('#mobileMenuButton').toggleClass('nav-mobile-button');
+		$('.nav').removeClass('nav-mobile');
+        $('.link-nav').removeClass('nav-el-mobile');
+    })
 });
 
 
